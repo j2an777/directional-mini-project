4f0aacfb009d9df2430d108648027809477cd4bd
j2an777 <ha991104@gmail.com>
2025-10-27 17:45:39 +0900

    chore: 필요없는 파일 및 주석 삭제

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,15 +16,11 @@ const eslintConfig = [
   },
   {
     rules: {
-      // TypeScript 관련 규칙 완화
-      '@typescript-eslint/no-explicit-any': 'warn', // any 타입 사용 시 경고만 표시
-      '@typescript-eslint/no-unused-vars': 'warn', // 사용하지 않는 변수 경고만 표시
-      '@typescript-eslint/no-non-null-assertion': 'warn', // non-null assertion 경고만 표시
-
-      // 개발 편의를 위한 규칙 완화
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
       'prefer-const': 'warn',
-      // "no-console": "warn", // console.log 사용 시 경고만 표시
-      '@typescript-eslint/no-explicit-any': 'off',
+      'no-console': 'warn',
     },
   },
 ];
