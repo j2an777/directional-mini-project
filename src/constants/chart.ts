a07ea40033d3f679525d6b464b15a69b302b9d78
j2an777 <ha991104@gmail.com>
2025-10-28 23:31:27 +0900

    feat: 더미 데이터 생성
    - 메뉴와 게시글, 차트에 필요한 더미 데이터 따로 분리를 해놨습니다.

---
+++ src/constants/chart.ts
@@ -0,0 +1,13 @@
+import { colors } from '@/styles/colorPalatte';
+
+const COLORS = {
+  happy: colors.pink,
+  tired: colors.blue30,
+  stressed: colors.orange30,
+};
+
+const PALETTE = [colors.green30, colors.orange30, colors.brown30, colors.pink, colors.purple30];
+
+const TEAM_COLORS = [colors.pink, colors.blue30, colors.green30, colors.orange30, colors.purple30, colors.green50];
+
+export { COLORS, PALETTE, TEAM_COLORS };
---
+++ src/constants/menu.ts
@@ -0,0 +1,9 @@
+const menuItems = [
+  {
+    id: 1,
+    name: '나의 게시글',
+    url: '/post',
+  },
+];
+
+export default menuItems;
