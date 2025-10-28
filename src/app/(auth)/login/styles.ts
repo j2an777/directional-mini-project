48f8a7202b3b19f96f44dba86ae1d3d7c3477c55
j2an777 <ha991104@gmail.com>
2025-10-28 23:24:10 +0900

    feat: 로그인 페이지 및 폼 컴포넌트 생성
    - 로그인 페이지 및 폼 컴포넌트 퍼블리싱 완료했습니다.
    - react hook form의 useForm을 이용하여 각 컴포넌트의 control로 값에 대해
      유효성 검사 및 입력되게 설계했습니다.

---
+++ src/app/(auth)/login/styles.ts
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export { Wrapper };
